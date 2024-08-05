dc98c96a68441adae98f25f020d2d4db829bb5b2
nernoel <142674871+nernoel@users.noreply.github.com.>
2024-08-05 15:12:52 -0400

    small changes

--- main.go
+++ main.go
@@ -1,33 +1,29 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"os"
 	"postgo/commands" // import file of commands
 	"time"
 
 	_ "github.com/lib/pq"
 )
 
-
+// HashMap to map keys to command strings
+var commandMap = new(map[rune]string)
 
 func main() {
+	// Define the database config for the user
 	dbConfig := &commands.DatabaseConfig{} // set db config to database config from commands
+	// Check if the database is connected
 	isConnected := commands.CheckDatabaseConn(dbConfig)
-	
-	/* Declaring buffered reader for user input */
-	reader := bufio.NewReader(os.Stdin)
-	fmt.Print("Welcome back!\n")
-	time.Sleep(5 * time.Second)
-	fmt.Print("Press h for any help on the custom commands!")
-	fmt.
 
-	/*
-	 * Continuously read commands from the user with while loop
-	*/
-	for {
-		//
-	} 
+	// If the database is successfully connected.. start prompting for commands
+	if isConnected == true {
+		time.Sleep(5 * time.Second) // Sleep for 5 seconds
+		fmt.Print("Welcome back!\n Start entering your commands.\nUse the h key for command help")
+		for {
 
+		}
+	}
 }
+
